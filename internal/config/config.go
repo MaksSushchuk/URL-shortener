05ebe7b3429adef35ecc9b30275814f238d17c46
Maks Sushchuk <maks313455@gmail.com>
2025-11-01 19:57:48 +0200

    Initialize project structure with basic configuration files and dependencies.

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-default:"development"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+}
+
+type HttpServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"10s"`
+}
+
+func MustLoad() *Config {
+	var configPath string = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("CONFIG_PATH does not exist")
+	}
+
+	var config Config
+
+	if err := cleanenv.ReadConfig(configPath, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	return &config
+}
